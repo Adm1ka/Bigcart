eb651cfd75e432115ce70edcc34c0a607b39f127
Adm1ka <lidina_nastya@mail.ru>
2025-06-10 00:33:51 +0600

    meow

--- app/src/main/java/com/example/bigcart/MyOrder.java
+++ app/src/main/java/com/example/bigcart/MyOrder.java
@@ -1,7 +1,10 @@
 package com.example.bigcart;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -10,8 +13,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MyOrder extends AppCompatActivity {
     public ImageView backarr, filter;
+    public BottomNavigationView bottomNavigationView;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,32 @@ public class MyOrder extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+            }
+        });
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_order) {
+                    Intent intent = new Intent(MyOrder.this, MyOrder.class);
+                    startActivity(intent);
+                    return true;
+                } else if (itemId == R.id.nav_home) {
+                    Intent intent = new Intent(MyOrder.this, Home.class);
+                    startActivity(intent);
+                    return true;}
+                else if (itemId == R.id.nav_profile) {
+                    Intent intent = new Intent(MyOrder.this, Profile.class);
+                    startActivity(intent);
+                    return true;
+                } else if (itemId == R.id.nav_cart) {
+                    Intent intent = new Intent(MyOrder.this, Cart.class);
+                    startActivity(intent);
+                    return true;
+                } else {
+                    return false;
+                }
             }
         });
     }
